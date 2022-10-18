bdb38257d405495beb04ce9f11a464884b425447
NutLenovoLaptop <nutdeknon@gmail.com>
2022-10-18 15:46:18 +0700

    replace image container with imageSkeleton

--- src/components/Thanks/Thanks.jsx
+++ src/components/Thanks/Thanks.jsx
@@ -1,12 +1,20 @@
 import classes from "./thanks.module.css";
 import ThanksImg from "../../assets/illustration-thank-you.svg";
+import { useState } from "react";
+
 export const Thanks = (props) => {
+  const [imageState, setImageState] = useState(false);
+
+  const imageLoadHandler = () => {
+    setImageState(true);
+  };
+
   const { score, maxScore } = props;
   return (
     <div>
-      <div className={classes["image-container"]}>
-        <img src={ThanksImg} alt="Thanks Image" />
-      </div>
+      {!imageState && <div className={classes.imageSkeleton}></div>}
+      <img src={ThanksImg} alt="Thanks Image" onLoad={imageLoadHandler} />
+
       <div className={classes["thanks-container"]}>
         <p>
           You selected {props.score || 0} out of {props.maxScore || 5}
